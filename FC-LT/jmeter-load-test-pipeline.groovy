41cb9919b81a43029da42253bd96bc5511fbe86d
Chandika Udaya Kumara <chandika@codegen.net>
2020-10-01 21:57:30 +0530

    test

--- FC-LT/jmeter-load-test-pipeline.groovy
+++ FC-LT/jmeter-load-test-pipeline.groovy
@@ -5,18 +5,25 @@ def interval = Integer.parseInt(params.INTERVAL)
 def testcaseList = params.TESTCASE.split(',')
 def rndResCnt = Integer.parseInt(params.RND_RES_CNT)
 def cnvId = params.CNV_ID
+int stageCount = 0;
 
-def runProject(stage_name, tc, duration, cnvId, threadCount, delay, rndResCnt){
+if(cnvId==""){
+    cnvId = "LT" + (new Date()).format("yyyyMMddHHmmss");
+}
+
+
+def runProject(stage_name, tc, duration, cnvId, threadCount, delay, rndResCnt, stageCount){
 //    def title = email_prefix +" "+ profile + " " + duration.toString() + "D " + (new Date()).format("yyyy-MM-dd HH:mm:ss") + " #${BUILD_NUMBER}"
     def title =  "${BUILD_NUMBER}##"
     def timeOut = duration + 5;
     duration = (duration*60).toString()
+    def _cnvId = cnvId + "-" + stageCount;
     def jmeter_home = "/home/jenkins/jmeter"
     stage stage_name + (threadCount==""?"":"-T"+threadCount.toString()) + (delay==""?"":"-D"+delay.toString())
     node {
         try{
             timeout(time: timeOut, unit: 'MINUTES') {
-                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log"
+                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log"
 
                 readFile("${jmeter_home}/prj/summary-report.csv").split('\n').each { line, count -> echo line }
             }
@@ -32,7 +39,8 @@ testcaseList.each {
         def threadCount = it;
         delayList.each {
             def delay = it;
-            runProject(tc, tc, duration, cnvId, threadCount, delay, rndResCnt)
+            stageCount++;
+            runProject(tc, tc, duration, cnvId, threadCount, delay, rndResCnt, stageCount)
         }
     }
     sleep interval
