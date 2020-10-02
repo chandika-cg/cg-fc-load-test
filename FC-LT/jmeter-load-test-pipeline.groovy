13d3b7fb152607505a17d8fddbeda60274861c78
Chandika Udaya Kumara <chandika@codegen.net>
2020-10-02 10:51:23 +0530

    -

--- FC-LT/jmeter-load-test-pipeline.groovy
+++ FC-LT/jmeter-load-test-pipeline.groovy
@@ -50,8 +50,8 @@ def runProject(stage_name, tc, duration, noResponse, cnvId, threadCount, delay, 
 //                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report-${executionId}.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JNO_RESPONSE=${noResponse} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log -e -o reports/${executionId}"
 
                 sh "mkdir -p summary"
-                sh "mv gen-summary-report.csv summary/summary-${executionId}.csv"
-                archiveArtifacts artifacts: "summary/summary-${executionId}.csv", excludes: 'reports/*.md'
+                sh "mv gen-summary-report.csv summary/summary-${executionId}.html"
+                archiveArtifacts artifacts: "summary/summary-${executionId}.html", excludes: 'reports/*.md'
 //                publishHTML (target: [
 //                        allowMissing: false,
 //                        alwaysLinkToLastBuild: false,
