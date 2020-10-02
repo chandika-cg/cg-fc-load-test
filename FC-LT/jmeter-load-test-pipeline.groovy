e261ac3066e90548b3b4bed645419c7b43a2812c
Chandika Udaya Kumara <chandika@codegen.net>
2020-10-02 08:11:07 +0530

    -

--- FC-LT/jmeter-load-test-pipeline.groovy
+++ FC-LT/jmeter-load-test-pipeline.groovy
@@ -7,8 +7,10 @@ def rndResCnt = Integer.parseInt(params.RND_RES_CNT)
 def cnvId = params.CNV_ID
 int stageCount = 0;
 
+def pipelineId = (new Date()).format("yyyyMMddHHmmss") + (Math.abs(new Random().nextInt() % [100]) + 1).toString();
+
 if(cnvId==""){
-    cnvId = "LT" + (new Date()).format("yyyyMMddHHmmss");
+    cnvId = "LT" + pipelineId;
 }
 
 def description = "";
@@ -29,6 +31,8 @@ def runProject(stage_name, tc, duration, cnvId, threadCount, delay, rndResCnt, s
     duration = (duration*60).toString();
     def _cnvId = cnvId + "-" + stageCount;
     def jmeter_home = "/home/jenkins/jmeter";
+    def workspace = "/home/jenkins/jmeter";
+    def executionId = pipelineId + stageCount.toString();
     stage stage_name + (threadCount==""?"":"-T"+threadCount.toString()) + (delay==""?"":"-D"+delay.toString())
     node {
         try{
@@ -41,8 +45,9 @@ def runProject(stage_name, tc, duration, cnvId, threadCount, delay, rndResCnt, s
                 description += "\n+  DELAY                   = ${delay}";
                 description += "\n+------------------------------------------------+";
                 echo description;
-
-                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report-${BUILD_NUMBER}.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log -e -o report"
+                sh "mkdir -p reports"
+                sh "mkdir -p reports/${executionId}"
+                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report-${BUILD_NUMBER}.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log -e -o reports/${executionId}"
 //                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log"
 
 //                publishHTML target: [
