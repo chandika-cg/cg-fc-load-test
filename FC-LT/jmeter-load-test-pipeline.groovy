8fcb30496d01d715a5e78671082f95c3035be7fe
Chandika Udaya Kumara <chandika@codegen.net>
2020-10-02 15:09:10 +0530

    -

--- FC-LT/jmeter-load-test-pipeline.groovy
+++ FC-LT/jmeter-load-test-pipeline.groovy
@@ -50,6 +50,7 @@ def runProject(stage_name, tc, duration, noResponse, cnvId, threadCount, delay, 
                 sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report-${executionId}.jtl -t ${jmeter_home}/prj/${jmx_file} -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JNO_RESPONSE=${noResponse} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log"
 //                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report-${executionId}.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JNO_RESPONSE=${noResponse} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log -e -o reports/${executionId}"
 
+                sh "java -version"
                 sh "mkdir -p summary"
                 sh "mv gen-summary-report.html summary/summary-${executionId}.html"
                 archiveArtifacts artifacts: "summary/summary-${executionId}.html", excludes: 'reports/*.md'
