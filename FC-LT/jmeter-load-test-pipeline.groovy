2329637c1491b923a9b2d396378b4b257adbbcd3
Chandika Udaya Kumara <chandika@codegen.net>
2020-10-02 07:59:45 +0530

    tst

--- FC-LT/jmeter-load-test-pipeline.groovy
+++ FC-LT/jmeter-load-test-pipeline.groovy
@@ -22,11 +22,13 @@ echo description;
 
 def runProject(stage_name, tc, duration, cnvId, threadCount, delay, rndResCnt, stageCount){
 //    def title = email_prefix +" "+ profile + " " + duration.toString() + "D " + (new Date()).format("yyyy-MM-dd HH:mm:ss") + " #${BUILD_NUMBER}"
-    def title =  "${BUILD_NUMBER}##"
+    def title =  "${BUILD_NUMBER}##";
+    def buildPath = "${JENKINS_HOME}/jobs/${PATH_TO_JOB}/builds/${BUILD_NUMBER}";
+    echo "BUILD_PATH: ${buildPath}"
     def timeOut = duration + 5;
-    duration = (duration*60).toString()
+    duration = (duration*60).toString();
     def _cnvId = cnvId + "-" + stageCount;
-    def jmeter_home = "/home/jenkins/jmeter"
+    def jmeter_home = "/home/jenkins/jmeter";
     stage stage_name + (threadCount==""?"":"-T"+threadCount.toString()) + (delay==""?"":"-D"+delay.toString())
     node {
         try{
@@ -40,9 +42,12 @@ def runProject(stage_name, tc, duration, cnvId, threadCount, delay, rndResCnt, s
                 description += "\n+------------------------------------------------+";
                 echo description;
 
-                //sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report-${BUILD_NUMBER}.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log -e -o report"
-                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log"
+                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report-${BUILD_NUMBER}.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log -e -o report"
+//                sh "${jmeter_home}/bin/jmeter.sh -n -l ${jmeter_home}/prj/summary-report.csv -t ${jmeter_home}/prj/FCTG-LT-PP.jmx -JRND_RES_CNT=${rndResCnt} -JCNV_ID=${_cnvId} -JTESTCASE=${tc} -JTHREADS=${threadCount} -JRAMPUP=${delay} -JDURATION=${duration} -JLOOP_COUNT=1 -JSTARTUP_DELAY=0 -j ${jmeter_home}/prj/jmeter.log"
 
+//                publishHTML target: [
+//
+//                ]
 //                readFile("${jmeter_home}/prj/summary-report.csv").split('\n').each { line, count -> echo line }
             }
         } catch (error) {
