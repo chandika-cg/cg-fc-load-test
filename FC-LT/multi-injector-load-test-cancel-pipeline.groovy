1ffd06e4bf2378fe48284c76fd52ea7cfa6208f5
Chandika Udaya Kumara <chandika@codegen.net>
2021-01-22 13:02:48 +0530

    cancell multi injector LT

--- FC-LT/multi-injector-load-test-cancel-pipeline.groovy
+++ FC-LT/multi-injector-load-test-cancel-pipeline.groovy
@@ -1,6 +1,4 @@
 
-
-
 node {
     try{
         params.REGIONS.split(',').each {
@@ -9,11 +7,13 @@ node {
             def regionUrl = regionData[1].trim();
             def regionToken = regionData[2].trim();
 
+            def
+
             echo regionCode
             echo regionUrl
             echo regionToken
 
-            def url = regionUrl + "job/EXECUTE-JM-LOADTEST/lastBuild/kill"
+            def url = regionUrl + "job/EXECUTE-JM-LOADTEST/lastBuild/stop"
 
             echo sh(script: "curl --location --request POST '$url' -u grinder:$regionToken", returnStdout: true).trim()
         }
