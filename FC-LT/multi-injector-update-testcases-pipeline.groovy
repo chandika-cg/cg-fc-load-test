2c2eb6ab4d5afa2d5a22d6897c2e200dd7e9fb75
Chandika Udaya Kumara <chandika@codegen.net>
2021-01-25 07:45:35 +0530

    multi injector update testcases added

---
+++ FC-LT/multi-injector-update-testcases-pipeline.groovy
@@ -0,0 +1,21 @@
+
+node {
+    try{
+        params.REGIONS.split(',').each {
+            def regionData = it.split("::")
+            def regionCode = regionData[0].trim();
+            def regionUrl = regionData[1].trim();
+            def regionToken = regionData[2].trim();
+
+            def url = regionUrl + "job/GET-JM-LT-TESTCASES//buildWithParameters?token=multiregion_test"
+
+
+            echo sh(script: "curl $url -u grinder:$regionToken", returnStdout: true).trim()
+
+        }
+
+    } catch (error) {
+        echo error
+    }
+}
+
