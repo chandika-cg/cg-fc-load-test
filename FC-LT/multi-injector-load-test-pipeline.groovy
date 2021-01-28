1434d975bc62702b1dd7efc2823cdea7e69d34ad
Chandika Udaya Kumara <chandika@codegen.net>
2021-01-28 11:38:24 +0530

    refresh LT

--- FC-LT/multi-injector-load-test-pipeline.groovy
+++ FC-LT/multi-injector-load-test-pipeline.groovy
@@ -1,6 +1,7 @@
 
 
 def cnvId = params.CNV_ID;
+def refreshInterval = Eval.me(params.REFRESH_INTERVAL);
 if(cnvId==""){
     cnvId = "LT" + (new Date()).format("yyyyMMddHHmmss") + (Math.abs(new Random().nextInt() % [100]) + 1).toString();
 }
@@ -11,57 +12,73 @@ echo "+-------------------------------------------------------------+";
 
 
 node {
+
     try{
-        params.REGIONS.split(',').each {
-            def regionData = it.split("::")
-            def regionCode = regionData[0].trim();
-            def regionUrl = regionData[1].trim();
-            def regionToken = regionData[2].trim();
+        int refreshI = 0;
+        do {
+            params.REGIONS.split(',').each {
+                def regionData = it.split("::")
+                def regionCode = regionData[0].trim();
+                def regionUrl = regionData[1].trim();
+                def regionToken = regionData[2].trim();
 
-            echo regionCode
-            echo regionUrl
-            echo regionToken
+                echo regionCode
+                echo regionUrl
+                echo regionToken
 
-            def url = regionUrl + "job/EXECUTE-JM-LOADTEST/buildWithParameters?token=multiregion_test"
+                def url = regionUrl + "job/EXECUTE-JM-LOADTEST/buildWithParameters?token=multiregion_test"
 
-            def params = [
-                    'DURATION' :params.DURATION,
-                    'TESTCASE' : params.TESTCASE,
-                    'RAMPUP' : params.RAMPUP,
-                    'DELAY' : params.DELAY,
-                    'THREADS' : params.THREADS,
-                    'CNV_ID' : cnvId + "-" + regionCode,
-                    'INTERVAL' : params.INTERVAL,
-                    'RND_RES_CNT' : params.RND_RES_CNT,
-                    'JMETER_HOME' : params.JMETER_HOME,
-                    'JMX_FILE' : params.JMX_FILE,
-                    'RESULT_COUNT' : params.RESULT_COUNT,
-                    'TIMEOUT' : params.TIMEOUT,
-                    'PRPP' : params.PRPP,
-                    'RUN_TESTCASES_PARALLAY' : params.RUN_TESTCASES_PARALLAY,
-            ]
+                def params = [
+                        'DURATION'              : params.DURATION,
+                        'TESTCASE'              : params.TESTCASE,
+                        'RAMPUP'                : refreshI==0 ? params.RAMPUP : "0",
+                        'DELAY'                 : params.DELAY,
+                        'THREADS'               : params.THREADS,
+                        'CNV_ID'                : cnvId + "-" + regionCode,
+                        'INTERVAL'              : params.INTERVAL,
+                        'RND_RES_CNT'           : params.RND_RES_CNT,
+                        'JMETER_HOME'           : params.JMETER_HOME,
+                        'JMX_FILE'              : params.JMX_FILE,
+                        'RESULT_COUNT'          : params.RESULT_COUNT,
+                        'TIMEOUT'               : params.TIMEOUT,
+                        'PRPP'                  : params.PRPP,
+                        'RUN_TESTCASES_PARALLAY': params.RUN_TESTCASES_PARALLAY,
+                ]
 
-            def dataCurl = ""
-            for( entry in params )
-            {
-                if(entry.key == 'TESTCASE')
-                {
-                    for( val in entry.value.toString().split(",") )
-                    {
-                        url += " --data " + entry.key + "=" + val + " ";
+                def dataCurl = ""
+                for (entry in params) {
+                    if (entry.key == 'TESTCASE') {
+                        for (val in entry.value.toString().split(",")) {
+                            url += " --data " + entry.key + "=" + val + " ";
+                        }
+                    } else {
+                        url += " --data " + entry.key + "=" + entry.value + " ";
+                        //java.net.URLEncoder.encode(entry.value, "UTF-8");
                     }
+                    refreshI++;
                 }
-                else
-                {
-                    url += " --data " + entry.key + "=" + entry.value + " "; //java.net.URLEncoder.encode(entry.value, "UTF-8");
-                }
+
+
+                echo sh(script: "curl $url -u grinder:$regionToken $dataCurl", returnStdout: true).trim()
+
             }
 
+            if(refreshInterval>0){
+                sleep refreshInterval
+                params.REGIONS.split(',').each {
+                    def regionData = it.split("::")
+                    def regionCode = regionData[0].trim();
+                    def regionUrl = regionData[1].trim();
+                    def regionToken = regionData[2].trim();
 
-            echo sh(script: "curl $url -u grinder:$regionToken $dataCurl", returnStdout: true).trim()
 
-        }
 
+                    def url = regionUrl + "job/EXECUTE-JM-LOADTEST/lastBuild/stop"
+
+                    echo sh(script: "curl --location --request POST '$url' -u grinder:$regionToken", returnStdout: true).trim()
+                }
+            }
+        } while (refreshInterval>0);
 
     } catch (error) {
         echo error
