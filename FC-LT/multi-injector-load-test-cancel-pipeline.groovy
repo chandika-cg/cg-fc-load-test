5e2ee8698235363ae9b2f5a30d253aa3af6e473e
Chandika Udaya Kumara <chandika@codegen.net>
2021-09-23 20:26:41 +0530

    adde TRY_COUNT

--- FC-LT/multi-injector-load-test-cancel-pipeline.groovy
+++ FC-LT/multi-injector-load-test-cancel-pipeline.groovy
@@ -1,22 +1,23 @@
-
 node {
-    try{
-        params.REGIONS.split(',').each {
-            def regionData = it.split("::")
-            def regionCode = regionData[0].trim();
-            def regionUrl = regionData[1].trim();
-            def regionToken = regionData[2].trim();
+    for (def i = 0; i < Eval.me(params.TRY_COUNT); i++) {
+        try {
+            params.REGIONS.split(',').each {
+                def regionData = it.split("::")
+                def regionCode = regionData[0].trim();
+                def regionUrl = regionData[1].trim();
+                def regionToken = regionData[2].trim();
 
 
 
-            def url = regionUrl + "job/EXECUTE-JM-LOADTEST/lastBuild/stop"
+                def url = regionUrl + "job/EXECUTE-JM-LOADTEST/lastBuild/stop"
 
-            echo sh(script: "curl --location --request POST '$url' -u grinder:$regionToken", returnStdout: true).trim()
+                echo sh(script: "curl --location --request POST '$url' -u grinder:$regionToken", returnStdout: true).trim()
+            }
+
+
+        } catch (error) {
+            echo error
         }
-
-
-    } catch (error) {
-        echo error
     }
 }
 
